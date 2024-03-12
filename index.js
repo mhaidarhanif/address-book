447907f846b3aaf246d0e01817577f5a6e54bf16
M Haidar Hanif <git@mhaidarhanif.com>
2024-03-12 23:02:35 +0700

    feat: prepare contacts data

---
+++ index.js
@@ -0,0 +1,23 @@
+const contacts = [
+  {
+    id: 1,
+    fullName: "M Haidar Hanif",
+    age: 30,
+    phone: "+621234567890",
+  },
+  {
+    id: 2,
+    fullName: "Elon Musk",
+    age: 50,
+    phone: "+1234567890",
+  },
+  {
+    id: 3,
+    fullName: "Jeff Bezos",
+    age: 52,
+    phone: "+1234567890",
+  },
+];
+
+console.log(contacts);
+console.log(contacts[0]);
